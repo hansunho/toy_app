482ce947b3a657ed498449ac9b499616d07ad12f
hansunho <hansunho@grinnell.edu>
2015-07-06 00:01:48 -0500

    add presence validation to the user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	has_many :microposts
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :age, presence: true
+	validates :address, presence: true
 end
